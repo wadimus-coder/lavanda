ceb37f397dfb91abd77f08fb144202da9b68434e
Wadim <grusa1360@gmail.com>
2025-03-01 13:32:39 +0200

    Second part -soname=var.so

--- app.c
+++ app.c
@@ -5,3 +5,7 @@ int main()
     printf("var = %d\n", var);
     return 0;
 }
+
+// gcc -shared var.c -o var.so -Wl,-soname=var.so
+// создание именщванной переменной
+// export LD_LIBRARY_PATH=/mnt/c/Sock_FdCatalog/059a-versions_Linux/var_so_2
